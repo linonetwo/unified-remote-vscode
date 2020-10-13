ea0ea04fa74e266a8b1bb49f651bbf3ac4f4f7fc
林东吴 <lindongwu@bytedance.com>
2020-10-13 14:16:12 +0800

    Update remote.lua

--- VSCode/remote.lua
+++ VSCode/remote.lua
@@ -3,12 +3,12 @@ local keyboard = libs.keyboard;
 
 --@help Press key stroke to copy
 actions.copy = function ()
-	keyboard.stroke("command", "c");
+	keyboard.stroke("lcommand", "c");
 end
 
 --@help Press key stroke to paste
 actions.paste = function ()
-	keyboard.stroke("command", "v");
+	keyboard.stroke("lcommand", "v");
 end
 
 --@help Press key stroke to format
